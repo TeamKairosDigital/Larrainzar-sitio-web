7bb33153185462c3161dedc6384847b5f650d6d7
TeamKairosDigital <kairos0698@gmail.com>
2025-01-30 15:17:33 -0600

    add HashLocationStrategy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...appConfig.providers,
-    provideRouter(routes),
-    provideAnimations(),  // Agregando BrowserAnimationsModule aquí
+    provideRouter(routes),  // Usar HashLocationStrategy
+    provideAnimations(),
     provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },  // Especificar HashLocationStrategy
   ]
 })
   .catch((err) => console.error(err));
